25edd6b9aeff568cc83ec031e3db5a32cc506126
Sami Emad <samiemad92@gmail.com>
2017-02-18 10:12:09 +0200

    change mail from address

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'antique@samiemad.me',
+        'name' => 'Antique',
     ],
 
     /*
